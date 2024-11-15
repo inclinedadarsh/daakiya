614213f53309e2952b3dbec7f54badf7fcf30135
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-11-15 21:24:32 +0530

    feat: add UI components and state management with Jotai for main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+import { urlAtom, responseAtom } from "./lib/atoms";
+import { useAtom } from "jotai";
+
 function App() {
-  return <h1>Hello world</h1>;
+  const [url, setUrl] = useAtom(urlAtom);
+  const [response, setResponse] = useAtom(responseAtom);
+
+  const handleClick = () => {
+    console.log(url);
+  };
+
+  return (
+    <main className='min-h-screen max-w-3xl mx-auto flex justify-center items-center gap-4 flex-col'>
+      <div className='flex justify-center items-end gap-4 w-full'>
+        <div className='grow'>
+          <Label htmlFor='url'>Input URL</Label>
+          <Input
+            value={url}
+            onChange={(e) => {
+              setUrl(e.target.value);
+            }}
+            id='url'
+            className='mt-2'
+          />
+        </div>
+        <Button onClick={handleClick}>Send Request</Button>
+      </div>
+      <p className='w-full min-h-40 border border-border rounded-md'>
+        {response}
+      </p>
+    </main>
+  );
 }
 
 export default App;
