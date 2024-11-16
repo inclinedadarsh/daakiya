177fb3455f184ad34c300e0db063e80d19b60d7d
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-11-16 12:30:32 +0530

    feat(server): add POST endpoint to fetch URL content with redirection handling

--- server/server/main.py
+++ server/server/main.py
@@ -1,8 +1,26 @@
+from pydantic import BaseModel
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
+
+
+class Url(BaseModel):
+    url: str
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/url")
+async def get_url(url: Url):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url.url)
+
+    if response.status_code == 308:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(response.headers["location"])
+
+    return {"response": response.text}
